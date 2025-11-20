22192cb43862cc8f3f42f96727e9a86019e08b7a
AbdallahElaraby <pipo.elaraby99@gmail.com>
2025-11-20 16:21:41 +0200

    seed edit

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
+VOTE_URL="${VOTE_SERVICE_URL:-http://vote-service:80}"
 # create 3000 votes (2000 for option a, 1000 for option b)
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
-ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote/
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" "$VOTE_URL/"
+ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" "$VOTE_URL/"
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" "$VOTE_URL/"
